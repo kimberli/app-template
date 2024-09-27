bf171d965c26c14b252d33bb83f27ba4488f9947
kimberli <kimberlizhong@gmail.com>
2024-09-26 20:25:25 -0700

    reorder imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-
 import "./globals.css";
 
 import type { Metadata } from "next";
+import React from "react";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://TODO-my-site.com"),
